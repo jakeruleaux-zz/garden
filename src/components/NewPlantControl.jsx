b2582627cf5d944606bd73b565ebbd0f705cdc80
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-15 09:21:54 -0700

    finish plant control file

--- src/components/NewPlantControl.jsx
+++ src/components/NewPlantControl.jsx
@@ -4,8 +4,39 @@ import PropTypes from "prop-types";
 class NewPlantControl extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props
+    this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
+    this.state = {formVisibleOnPage: false};
+  }
+
+  showForm(event) {
+  this.setState({formVisibleOnPage: true});
+  }
+
+  hideForm(event) {
+  this.setState({formVisibleOnPage: false});
+  }
+
+  render() {
+    let formAreaContent = null;
+    if (this.state.formVisibleOnPage) {
+      formAreaContent = <NewPlant onNewPlantCreation={this.props.onNewPlantCreation} hideFormAfterSubmission = {this.hideForm}/>
+    } else {
+      formAreaContent = <button onClick={this.showForm}>Add a Plant</button>;
+    }
+
+    return(
+      <div>
+        {formAreaContent}
+      </div>
+    );
   }
 }
+
+NewPlantControl.propTypes = {
+  onNewPlantCreation: PropTypes.func,
+}
+
 
 export default NewPlantControl;
