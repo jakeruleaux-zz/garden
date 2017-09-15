2e6a98e304ed5a7de4c4b5fd10587eb7ffc668a1
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-15 09:09:42 -0700

    begin build out of plant control file

---
+++ src/components/Garden.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from  "prop-types";
+
+class Garden extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Garden;
---
+++ src/components/NewPlantControl.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class NewPlantControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+}
+
+export default NewPlantControl;
