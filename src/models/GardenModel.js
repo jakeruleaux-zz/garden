14767b2e31ec9ef8de00ce13aafdd732f0fcbdd2
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-15 10:16:37 -0700

    refactor model to include moment

--- src/models/GardenModel.js
+++ src/models/GardenModel.js
@@ -1,9 +1,17 @@
+import Moment from "moment";
+
 class GardenModel {
   constructor(plantname, type, water, cost) {
     this.plantname = plantname;
     this.type = type;
     this.water = water;
     this.cost = cost;
+    this.timeWatered = new Moment();
+    this.timeSinceWatered = this.setTimeSinceWatered();
+  }
+
+  setTimeSinceWatered() {
+    this.timeSinceWatered = this.timeWatered.fromNow(true);
   }
 }
 
